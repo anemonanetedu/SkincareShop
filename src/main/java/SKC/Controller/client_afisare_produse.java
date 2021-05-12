5ba530852ea5de2bd1c13b22acb924cd474dcd68
CosminLiuba <cosmin.liuba@student.upt.ro>
2021-05-13 00:41:21 +0300

    Vizualizare lista produse de catre client

---
+++ src/main/java/SKC/Controller/client_afisare_produse.java
@@ -0,0 +1,43 @@
+package SKC.Controller;
+
+import SKC.Model.Produs;
+import SKC.Services.ProduseService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+public class client_afisare_produse {
+
+    @FXML
+    private TableView<Produs> tabelproduse;
+    @FXML
+    private TableColumn<Produs, String> nume;
+    @FXML
+    private TableColumn<Produs, String> pret;
+    @FXML
+    private TableColumn<Produs, String> tippiele;
+
+    public void initialize() {
+        nume.setCellValueFactory(new PropertyValueFactory<>("nume"));
+        pret.setCellValueFactory(new PropertyValueFactory<>("pret"));
+        tippiele.setCellValueFactory(new PropertyValueFactory<>("tippiele"));
+
+        tabelproduse.setItems(produse);
+
+    }
+
+    ObservableList<Produs> produse = FXCollections.observableArrayList(ProduseService.getProduse());
+
+    public void handleButonInapoi() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("client_main_page.fxml"));
+        Stage stage = (Stage) tabelproduse.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
+}
--- src/main/java/SKC/Services/ProduseService.java
+++ src/main/java/SKC/Services/ProduseService.java
@@ -4,8 +4,14 @@ import SKC.Exception.NoUpperCaseException;
 import SKC.Exception.UncompletedFieldsException;
 import SKC.Exception.UsernameAlreadyExistException;
 import SKC.Model.Produs;
+import SKC.Model.User;
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.objects.ObjectRepository;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static SKC.Services.FileSystemService.getPathProdusToFile;
 
@@ -21,14 +27,25 @@ public class ProduseService {
         produsRepository = database.getRepository(Produs.class);
     }
 
-
-
     public static void addProduse(String nume, String pret, String tippiele)
     {
 
      Produs produs = new Produs(nume,pret,tippiele);
         produsRepository.insert(produs);
 
+    }
+    public static void getProdusePreturi(ObservableList<String> produse){
+        for(Produs produs: produsRepository.find()) {
+            if(produs.getNume()!=null)
+            produse.add(produs.getNume()+" "+produs.getPret()+" "+produs.getTippiele());
+        }
+    }
+
+    public static ArrayList<Produs> getProduse(){
+        ArrayList<Produs> produse = new ArrayList<>();
+        for(Produs produs : produsRepository.find())
+            produse.add(produs);
+        return produse;
     }
 
 }
