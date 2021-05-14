7fc8ae9f75990c6c0c1297adfcb000047323cc44
CosminLiuba <cosmin.liuba@student.upt.ro>
2021-05-14 18:16:25 +0300

    clientii pot sa vada ce au in cos

---
+++ src/main/java/SKC/Controller/client_afisare_cos.java
@@ -0,0 +1,45 @@
+package SKC.Controller;
+
+import SKC.Model.Produs;
+import SKC.Services.CartServices;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class client_afisare_cos {
+    @FXML
+    private TableView<Produs> tabelproduse;
+    @FXML
+    private TableColumn<Produs, String> nume;
+    @FXML
+    private TableColumn<Produs, String> pret;
+    @FXML
+    private TableColumn<Produs, String> tippiele;
+
+    public void initialize() {
+        nume.setCellValueFactory(new PropertyValueFactory<>("nume"));
+        pret.setCellValueFactory(new PropertyValueFactory<>("pret"));
+        tippiele.setCellValueFactory(new PropertyValueFactory<>("tippiele"));
+        tabelproduse.setItems(produse);
+    }
+
+    ObservableList<Produs> produse = FXCollections.observableArrayList(CartServices.getProduse());
+
+
+    public void handleButonInapoi(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("client_main_page.fxml"));
+        Stage stage = (Stage) tabelproduse.getScene().getWindow();
+        stage.setScene(new Scene(root));
+
+    }
+}
--- src/main/java/SKC/Controller/client_main_page.java
+++ src/main/java/SKC/Controller/client_main_page.java
@@ -1,15 +1,20 @@
 package SKC.Controller;
 
 //import com.sun.javafx.tk.quantum.PaintRenderJob;
+import SKC.Model.Produs;
 import SKC.Model.User;
+import SKC.Services.CartServices;
 import SKC.Services.ProduseService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import javafx.scene.control.*;
 import javafx.collections.FXCollections;
@@ -143,7 +148,12 @@ public class client_main_page {
 
     }
 
-    public void handleVizualizareCos() {
+
+    public void handleVizualizareCos() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("client_afisare_cos.fxml"));
+        Stage stage = (Stage) (butonAlegereAngajat.getScene().getWindow());
+        stage.setScene(new Scene(root));
+
     }
 }
 
